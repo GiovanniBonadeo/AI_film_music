31626981f02593399842b68a3866bc5c832a05fd
Giovanni Bonadeo <giobonadeo00@gmail.com>
2024-08-04 15:59:22 +0200

    adding choose_base_note.m

--- add_mode_to_mel.m
+++ add_mode_to_mel.m
@@ -1,6 +1,38 @@
-function moded_mel = add_mode_to_mel(new_mel_seq, mode)
-% takes a sequence of grades as an array and generates the moledy according
+function moded_mel = add_mode_to_mel(gen_mel_seq, mode)
+% takes a sequence of grades  as an array and generates the moledy according
 % to the specified mode
-moded_mel = [];
+
+ % Define the modes intervals (in semitones) starting from Ionian (C major)
+    
+    all_modes = struct( ...
+        'ionian', [0 2 4 5 7 9 11 12 14 16 17 19 21 23], ... % Ionian (C major)
+        'dorian', [0 2 3 5 7 9 10 12 14 15 17 19 21 22], ... % Dorian
+        'phrygian', [0 1 3 5 7 8 10 12 13 15 17 19 20 22], ... % Phrygian
+        'lydian', [0 2 4 6 7 9 11 12 14 16 18 19 21 23], ... % Lydian
+        'mixolydian', [0 2 4 5 7 9 10 12 14 16 17 19 21 22], ... % Mixolydian
+        'aeolian', [0 2 3 5 7 8 10 12 14 15 17 19 20 22], ... % Aeolian (natural minor)
+        'locrian', [0 1 3 5 6 8 10 12 13 15 17 18 20 22] ... % Locrian
+    );
+
+    if ~isfield(all_modes, mode)
+        error('Invalid mode specified.');
+    end
+
+    mode_intervals = all_modes.(mode);
+
+    moded_mel = [];
+
+    c_midi = 60;
+
+    for i = 1:length(gen_mel_seq)
+     
+        grade = gen_mel_seq(i);
+        
+        if grade < 1 || grade > 14
+            error('Scale degree %d is out of valid range (1-14).', degree);
+        end
+        new_note = c_midi + mode_intervals(grade);
+        moded_mel = [moded_mel, new_note];
+    end
 
 end
---
+++ choose_base_note.m
@@ -0,0 +1,38 @@
+function note_based_mel = choose_base_note(mel_seq, base_note)
+%takes a sequence of midi pitches in C and remaps it on the base note
+    
+    note_based_mel = [];
+    transp_value = 0;
+
+    switch base_note
+        case {'C'}
+            transp_value = 0;
+        case {'C#', 'Db'}
+            transp_value = 1;
+        case {'D'}
+            transp_value = 2;
+        case {'D#', 'Eb'}
+            transp_value = 3;
+        case {'E'}
+            transp_value = 4;
+        case {'F'}
+            transp_value = 5;
+        case {'F#', 'Gb'}
+            transp_value = 6;
+        case {'G'}
+            transp_value = 7;
+        case {'G#', 'Ab'}
+            transp_value = 8;
+        case {'A'}
+            transp_value = 9;
+        case {'A#', 'Bb'}
+            transp_value = 10;
+        case {'B', 'Cb'}
+            transp_value = 11;
+    end
+
+    for i = 1:length(mel_seq)
+
+        note_based_mel = [note_based_mel, mel_seq(i) + transp_value];
+    end
+end
